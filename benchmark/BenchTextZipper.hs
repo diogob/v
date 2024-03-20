12bdd1e7ca8184d2861a2665ebc3d40033efded4
Diogo Biazus <diogo@biazus.ca>
2024-03-20 14:29:01 -0400

    Add vector to benchmarks

--- benchmark/BenchTextZipper.hs
+++ benchmark/BenchTextZipper.hs
@@ -7,6 +7,7 @@ import qualified Data.Text.Lazy.Zipper
 import qualified Data.Text.Rope.Zipper as R
 import Data.Text.Zipper (insertChar)
 import qualified Data.Text.Zipper as Z
+import qualified Data.Vector as V
 import HighlightedText (HighlightedText (HighlightedText))
 import qualified HighlightedText as H
 import qualified HighlightedText.Internal as H
@@ -28,31 +29,34 @@ insertChars = flip $ foldl' (flip Z.insertChar)
 insertRChars :: [Char] -> R.RopeZipper -> R.RopeZipper
 insertRChars = flip $ foldl' (flip R.insertChar)
 
-benchAll (highlightedBench, textBench, ropeBench, stringBench) =
+benchAll (highlightedBench, textBench, ropeBench, stringBench, vectorBench) =
   [ bench "HighlightedText" $ nf highlightedBench initialHZipper,
     bench "Text" $ nf textBench initialTZipper,
     bench "Rope" $ nf ropeBench initialRZipper,
-    bench "String" $ nf stringBench initialSZipper
+    bench "String" $ nf stringBench initialSZipper,
+    bench "Vector" $ nf vectorBench initialVZipper
   ]
   where
     initialHZipper = H.highlightedZipper [fromString testContent] Nothing
     initialTZipper = Z.textZipper [fromString testContent] Nothing
     initialSZipper = Z.stringZipper [fromString testContent] Nothing
     initialRZipper = fromString testContent :: R.RopeZipper
+    initialVZipper = Z.vectorZipper [toVector testContent] Nothing
+    toVector s = foldl' (<>) V.empty $ fmap V.singleton s
 
 main :: IO ()
 main =
   defaultMain
     [ bgroup
         "Identity"
-        $ benchAll (id, id, id, id),
+        $ benchAll (id, id, id, id, id),
       bgroup
         "Append 1X"
-        $ benchAll (insertChars testContent, insertChars testContent, insertRChars testContent, insertChars testContent),
+        $ benchAll (insertChars testContent, insertChars testContent, insertRChars testContent, insertChars testContent, insertChars testContent),
       bgroup
         "Append 2X"
-        $ benchAll (insertChars testContent2x, insertChars testContent2x, insertRChars testContent2x, insertChars testContent2x),
+        $ benchAll (insertChars testContent2x, insertChars testContent2x, insertRChars testContent2x, insertChars testContent2x, insertChars testContent2x),
       bgroup
         "Append 4X"
-        $ benchAll (insertChars testContent4x, insertChars testContent4x, insertRChars testContent4x, insertChars testContent4x)
+        $ benchAll (insertChars testContent4x, insertChars testContent4x, insertRChars testContent4x, insertChars testContent4x, insertChars testContent4x)
     ]
